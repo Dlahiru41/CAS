7cf43c6bb5c1a340bef51d49c4b794a56f121090
Dlahiru41 <dlahiru41@outlook.com>
2025-02-17 21:18:56 +0530

    Initial commit

---
+++ cas-common/src/main/java/org/finance/cascommon/CasCommonApplication.java
@@ -0,0 +1,13 @@
+package org.finance.cascommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasCommonApplication.class, args);
+    }
+
+}
---
+++ cas-server/src/test/java/org/finance/casserver/CasServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.finance.casserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
